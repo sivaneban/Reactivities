1c39f80991d4671d3a37bfc9659c05fdffa87011
unknown <sivaneban@gmail.com>
2020-10-16 12:50:59 +0530

    React CRUD functionalities

---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,11 @@
+import { Interface } from "readline";
+
+export interface IActivity{
+    id:string;
+    title:string;
+    description:string;
+    category:string;
+    date:string;
+    city:string;
+    venue:string;
+}
